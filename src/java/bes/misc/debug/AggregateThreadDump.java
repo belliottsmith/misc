da87a6ff2b5551ac525d69291aeae2b31f96d471
Benedict Elliott Smith <benedict@apache.org>
2015-01-21 00:25:47 +0000

    count threads and group thread states together

--- src/java/bes/misc/debug/AggregateThreadDump.java
+++ src/java/bes/misc/debug/AggregateThreadDump.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,8 +19,11 @@ public class AggregateThreadDump
                                                  "|at " +
                                                  ").*");
 
+    static final Pattern STATE = Pattern.compile("\\s*java\\.lang\\.Thread\\.State: ([A-Z_]+).*");
+
     static final Pattern REPLACE = Pattern.compile("<[0-9a-fx]+>");
 
+    // TODO: merge stack traces together
     public static void main(String[] args) throws IOException
     {
         if (args.length == 0)
@@ -37,11 +37,12 @@ public class AggregateThreadDump
             fail();
         }
 
-        Map<String, Collection<String>> traceToThread = new LinkedHashMap<>();
+        Map<String, Map<String, Collection<String>>> stateToTraceToThread = new LinkedHashMap<>();
         {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String thread = null;
-            StringBuffer trace = new StringBuffer();
+            String state = null;
+            StringBuilder trace = new StringBuilder();
             for (String line = reader.readLine() ; line != null ; line = reader.readLine())
             {
                 Matcher m = TRACE.matcher(line);
@@ -53,30 +54,58 @@ public class AggregateThreadDump
                     }
                     else
                     {
+                        if (state == null)
+                            state = "";
                         String fullTrace = trace.toString();
                         trace.setLength(0);
+                        Map<String, Collection<String>> traceToThread = stateToTraceToThread.get(state);
+                        if (traceToThread == null)
+                            stateToTraceToThread.put(state, traceToThread = new LinkedHashMap<>());
                         Collection<String> threads = traceToThread.get(fullTrace);
                         if (threads == null)
                             traceToThread.put(fullTrace, threads = new TreeSet<>());
                         threads.add(thread);
                         thread = null;
+                        state = null;
                     }
                 }
                 else
                 {
                     m = REPLACE.matcher(line);
-                    trace.append(m.replaceAll("?"));
+                    line = m.replaceAll("?");
+                    m = STATE.matcher(line);
+                    if (m.matches())
+                        state = m.group(1);
+                    trace.append(line);
                     trace.append("\n");
                 }
             }
         }
-        for (Map.Entry<String, Collection<String>> e : traceToThread.entrySet())
+
+        LinkedHashSet<String> states = new LinkedHashSet<>();
+        states.addAll(Arrays.asList("RUNNABLE", "TIMED_WAITING", "WAITING"));
+        states.addAll(stateToTraceToThread.keySet());
+        for (String state : states)
         {
-            System.out.println("\n======================\nTrace:");
-            System.out.println(e.getKey());
-            System.out.printf("Threads:\n");
-            for (String thread : e.getValue())
-                System.out.printf("\t%s\n", thread);
+            List<Map.Entry<String, Collection<String>>> traces = new ArrayList<>(stateToTraceToThread.get(state).entrySet());
+            Collections.sort(traces, new Comparator<Map.Entry<String, Collection<String>>>()
+            {
+                public int compare(Map.Entry<String, Collection<String>> o1, Map.Entry<String, Collection<String>> o2)
+                {
+                    return o2.getValue().size() - o1.getValue().size();
+                }
+            });
+            int runningTotalThreads = 0;
+            for (Map.Entry<String, Collection<String>> e : traces)
+            {
+                System.out.println("\n======================\nTrace:");
+                System.out.println(e.getKey());
+                int threads = e.getValue().size();
+                runningTotalThreads += threads;
+                System.out.printf("Threads (%d/%d):\n", threads, runningTotalThreads);
+                for (String thread : e.getValue())
+                    System.out.printf("\t%s\n", thread);
+            }
         }
     }
 
